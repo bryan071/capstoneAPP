261a011a2d5c4a6955a97e2e8d32eb941d8bba7e
Bry <gmbryan07@gmail.com>
2025-09-24 20:04:43 +0800

    update

--- app/src/main/java/com/project/webapp/api/WeatherService.kt
+++ app/src/main/java/com/project/webapp/api/WeatherService.kt
@@ -46,7 +46,7 @@ fun WeatherSection(context: Context) {
     val fusedLocationProviderClient =
         remember { LocationServices.getFusedLocationProviderClient(context) }
     val scope = rememberCoroutineScope()
-    val apiKey = "5b7853c5cd0b459f87a63035253003"
+    val apiKey = "a40a45c906a747b5a57123318251808"
 
     var cityName by remember { mutableStateOf("Fetching location...") }
     var temperature by remember { mutableStateOf("Fetching...") }
